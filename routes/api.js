71ff52b10366f70a6636c86ae87577269aecc25e
WilliamTempleton <william.templeton@centrale-marseille.fr>
2020-02-16 22:40:48 +0100

    message d'erreur si pas entier positif

--- routes/api.js
+++ routes/api.js
@@ -3,10 +3,15 @@ const listePremiers = require('../liste_nbr_prms');
 const router = express.Router();
 
 router.post('/calculs', function(req,res){
-    res.send({
-        squareRoot: Math.sqrt(req.body.nombre),
-        primes: listePremiers(req.body.nombre)
-    })
+    if (req.body.nombre > 0 && Number.isInteger(req.body.nombre)){
+        res.send({
+            squareRoot: Math.sqrt(req.body.nombre),
+            primes: listePremiers(req.body.nombre)
+        })
+    }
+    else {
+        res.status(400).send({error: "Fournir un entier positif"})
+    }
 });
 
 module.exports = router;
